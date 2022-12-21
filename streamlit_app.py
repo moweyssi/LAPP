bdde91fd9fed1794f976ba65936b39bba3a7d795
Maxim <maxim.oweyssi@est.org.uk>
2022-12-21 17:40:49 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,15 +3,24 @@ import streamlit.components.v1 as components
 import mechanize
 from bs4 import BeautifulSoup
 
-br = mechanize.Browser()
-br.set_handle_robots(False)
-br.addheaders = [('user-agent', '  Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.2.3) Gecko/20100423 Ubuntu/10.04 (lucid) Firefox/3.6.3')]
-br.open("https://eaccess.dumgal.gov.uk/online-applications/search.do?action=advanced&searchType=Application")
-# follow second link with element text matching regular expression
-br.select_form('searchCriteriaForm')
-br['searchCriteria.reference'] = "18/1706/FUL"
-br.submit()
-soup = BeautifulSoup(br.response().read())
-print(soup)
-st.markdown(soup,unsafe_allow_html=True)
+#br = mechanize.Browser()
+#br.set_handle_robots(False)
+#br.addheaders = [('user-agent', '  Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.2.3) Gecko/20100423 Ubuntu/10.04 (lucid) Firefox/3.6.3')]
+#br.open("https://eaccess.dumgal.gov.uk/online-applications/search.do?action=advanced&searchType=Application")
+## follow second link with element text matching regular expression
+#br.select_form('searchCriteriaForm')
+#br['searchCriteria.reference'] = "18/1706/FUL"
+#br.submit()
+#soup = BeautifulSoup(br.response().read())
+#print(soup)
+#st.markdown(soup,unsafe_allow_html=True)
 #components.html(html=soup,height=600)
+
+import mechanicalsoup
+browser = mechanicalsoup.StatefulBrowser()
+browser.open("https://eaccess.dumgal.gov.uk/online-applications/search.do?action=advanced&searchType=Application")
+browser.select_form()
+browser['searchCriteria.reference'] = "18/1706/FUL"
+browser.submit_selected()
+#browser.launch_browser()
+"""done"""
