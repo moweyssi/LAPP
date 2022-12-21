49c4fb2b7fbcddc4790af54c1e91cf30c8cd127f
Maxim <maxim.oweyssi@est.org.uk>
2022-12-21 17:22:59 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 
 br = mechanize.Browser()
 br.set_handle_robots(False)
+br.addheaders = [('user-agent', '  Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.2.3) Gecko/20100423 Ubuntu/10.04 (lucid) Firefox/3.6.3')]
 br.open("https://eaccess.dumgal.gov.uk/online-applications/search.do?action=advanced&searchType=Application")
 # follow second link with element text matching regular expression
 br.select_form('searchCriteriaForm')
