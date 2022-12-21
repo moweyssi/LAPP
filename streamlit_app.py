47ebb12d61f8fcd1fcfe9c8e91cb4a00d1ef08b9
Maxim <maxim.oweyssi@est.org.uk>
2022-12-21 17:13:21 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,14 @@
 import streamlit as st
 import streamlit.components.v1 as components
+import mechanize
+from bs4 import BeautifulSoup
 
-# embed streamlit docs in a streamlit app
-#components.iframe("https://eaccess.dumgal.gov.uk/")
-components.html(html=
-"""
-<center>
-    <h1 style="color:green">Talkers code</h1>
-    <!-- this is only for educational purposes -->
-    <p>Alternative of iframe tag</p>
-    <embed src="https://eaccess.dumgal.gov.uk/online-applications/search.do?action=advanced" width="1000" height="500" style="border: 1px solid white; box-shadow: 0 0 5px black;" />
-</center>""",
-height=600)
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.open("https://eaccess.dumgal.gov.uk/online-applications/search.do?action=advanced&searchType=Application")
+# follow second link with element text matching regular expression
+br.select_form('searchCriteriaForm')
+br['searchCriteria.reference'] = "18/1706/FUL"
+br.submit()
+soup = BeautifulSoup(br.response().read())
+components.html(html=soup,height=600)
