0a35ebefbdf1001910f51b7efa06d7cdf9a1cde8
Maxim <maxim.oweyssi@est.org.uk>
2022-12-21 19:13:19 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ highland = "https://wam.highland.gov.uk/wam/search.do?action=advanced"
 argbute = "https://publicaccess.argyll-bute.gov.uk/online-applications/search.do?action=advanced"
 renfrewshire = "https://pl-bs.renfrewshire.gov.uk/online-applications/search.do?action=advanced"
 fife = "https://planning.fife.gov.uk/online/search.do?action=advanced"
+
 authorities = ["Dumfries and Galloway","Highland","Argyll and Bute","Renfrewshire","Fife"]
 links = [dumgal,highland,argbute,renfrewshire,fife]
 choice = st.selectbox("Local Authority",authorities)
@@ -15,4 +16,6 @@ choice = st.selectbox("Local Authority",authorities)
 #    st.markdown("[LAPP](%s)" % links[1])
 #elif choice == authorities[2]:
 #    st.markdown("[LAPP](%s)" % links[3])
-[st.write("[LAPP](%s)" % links[i]) for i in range(len(links)) if choice==authorities[i]]
+for i in range(len(links)):
+    if choice==authorities[i]:
+        st.write("[LAPP](%s)" % links[i])  
