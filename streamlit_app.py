491e72d9381ec18885c4253e16940786a02742bf
Maxim <maxim.oweyssi@est.org.uk>
2022-12-21 14:49:00 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,14 +4,9 @@ import streamlit.components.v1 as components
 # embed streamlit docs in a streamlit app
 #components.iframe("https://eaccess.dumgal.gov.uk/")
 components.html(html="""
-        <head>
-          <title> Title of the document<title>
-        </head>
-          <body>
     <center>
         <h1 style="color:green">Talkers code</h1>
         <!-- this is only for educational purposes -->
         <p>Alternative of iframe tag</p>
         <embed src="https://www.talkerscode.com" width="1000" height="500" style="border: 1px solid white; box-shadow: 0 0 5px black;" />
-    </center>
-   </body>""",height=600)
+    </center>""")
