034265f9d9b7c09a18f0dcecfe13e5060ff02233
Maxim <maxim.oweyssi@est.org.uk>
2022-12-21 18:45:20 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,4 @@ highland = "https://wam.highland.gov.uk/wam/search.do?action=advanced"
 argbute = "https://publicaccess.argyll-bute.gov.uk/online-applications/search.do?action=advanced"
 st.selectbox("Local Authority",["Dumfries and Galloway","Highland","Argyll and Bute"])
 
-st.markdown(f'''
-<a href={dumgal}><button style="background-color:GreenYellow;">Stackoverflow</button></a>
-''',
-unsafe_allow_html=True)
+st.markdown("check out this [link](%s)" % dumgal)
