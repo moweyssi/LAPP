fda3cb52ca61e0a545cf478456657826a6db72bf
Maxim <maxim.oweyssi@est.org.uk>
2022-12-21 19:28:37 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,17 +8,16 @@ fife = "https://planning.fife.gov.uk/online/search.do?action=advanced"
 borders = "https://eplanning.scotborders.gov.uk/online-applications/search.do?action=advanced"
 angus = "https://planning.angus.gov.uk/online-applications/search.do?action=advanced"
 tayside = "https://idoxwam.dundeecity.gov.uk/idoxpa-web/search.do?action=advanced"
+strathclyde = "https://publicaccess.glasgow.gov.uk/online-applications/search.do?action=advanced"
+grampian = "https://publicaccess.aberdeencity.gov.uk/online-applications/search.do?action=advanced"
+westlothian = "https://planning.westlothian.gov.uk/publicaccess/search.do?action=advanced"
+midlothian = "https://planning-applications.midlothian.gov.uk/OnlinePlanning/search.do?action=advanced"
+eastlothian = "https://pa.eastlothian.gov.uk/online-applications/search.do?action=advanced"
 
-authorities = ["Dumfries and Galloway","Highland","Argyll and Bute","Renfrewshire","Fife","Borders","Angus","Tayside/Dundee"]
-links = [dumgal,highland,argbute,renfrewshire,fife,borders,angus,tayside]
+authorities = ["Dumfries and Galloway","Highland","Argyll and Bute","Renfrewshire","Fife","Borders","Angus","Tayside/Dundee","Strathclyde/Glasgow","Grampian/Aberdeen","West Lothian","Midlothian","East Lothian"]
+links = [dumgal,highland,argbute,renfrewshire,fife,borders,angus,tayside,strathclyde,grampian,westlothian,midlothian,eastlothian]
 choice = st.selectbox("Local Authority",authorities)
 
-#if choice == authorities[0]:
-#    st.markdown("[LAPP](%s)" % links[0])
-#elif choice == authorities[1]:
-#    st.markdown("[LAPP](%s)" % links[1])
-#elif choice == authorities[2]:
-#    st.markdown("[LAPP](%s)" % links[3])
 for i in range(len(links)):
     if choice==authorities[i]:
         st.write("[LAPP](%s)" % links[i])  
