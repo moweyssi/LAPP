d302625ebf5eb9408948b2caacc3fb2753a37bc0
Maxim <maxim.oweyssi@est.org.uk>
2022-12-21 19:03:38 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,16 @@ import webbrowser
 dumgal = "https://eaccess.dumgal.gov.uk/online-applications/search.do?action=advanced&searchType=Application"
 highland = "https://wam.highland.gov.uk/wam/search.do?action=advanced"
 argbute = "https://publicaccess.argyll-bute.gov.uk/online-applications/search.do?action=advanced"
-choice = st.selectbox("Local Authority",["Dumfries and Galloway","Highland","Argyll and Bute"])
+renfrewshire = "https://pl-bs.renfrewshire.gov.uk/online-applications/search.do?action=advanced"
+fife = "https://planning.fife.gov.uk/online/search.do?action=advanced"
+authorities = ["Dumfries and Galloway","Highland","Argyll and Bute","Renfrewshire","Fife"]
+links = [dumgal,highland,argbute,renfrewshire,fife]
+choice = st.selectbox("Local Authority",authorities)
 
-if choice == "Dumfries and Galloway":
-    st.markdown("[LAPP](%s)" % dumgal)
-elif choice == "Highland":
-    st.markdown("[LAPP](%s)" % highland)
+#if choice == authorities[0]:
+#    st.markdown("[LAPP](%s)" % links[0])
+#elif choice == authorities[1]:
+#    st.markdown("[LAPP](%s)" % links[1])
+#elif choice == authorities[2]:
+#    st.markdown("[LAPP](%s)" % links[3])
+[st.markdown("[LAPP](%s)" % links[i]) for i in range(len(links)) if choice==authorities[i]]
