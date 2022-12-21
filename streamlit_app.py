a88eec7f7394c5f7fd9ed463fc5aa789bde6dc29
Maxim <maxim.oweyssi@est.org.uk>
2022-12-21 14:39:20 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 # embed streamlit docs in a streamlit app
-components.iframe("https://en.wikipedia.org/wiki/Frame_(World_Wide_Web)")
+components.iframe("https://eaccess.dumgal.gov.uk/")
