e6780ed062a73d8c1c2942a7fc0e2bbff6cde89b
Maxim <maxim.oweyssi@est.org.uk>
2022-12-21 18:47:54 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,9 @@ import webbrowser
 dumgal = "https://eaccess.dumgal.gov.uk/online-applications/search.do?action=advanced&searchType=Application"
 highland = "https://wam.highland.gov.uk/wam/search.do?action=advanced"
 argbute = "https://publicaccess.argyll-bute.gov.uk/online-applications/search.do?action=advanced"
-st.selectbox("Local Authority",["Dumfries and Galloway","Highland","Argyll and Bute"])
+choice = st.selectbox("Local Authority",["Dumfries and Galloway","Highland","Argyll and Bute"])
 
-st.text("[LAPP](%s)" % dumgal)
+if choice == "Dumfries and Galloway":
+    st.markdown("[LAPP](%s)" % dumgal)
+elif choice == "Highland":
+    st.markdown("[LAPP](%s)" % highland)
