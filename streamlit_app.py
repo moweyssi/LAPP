9ba644faed1479becea78ac56f99f69d102ca4f6
Maxim <maxim.oweyssi@est.org.uk>
2022-12-21 18:39:47 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import webbrowser
 
-dumgal = ("https://eaccess.dumgal.gov.uk/online-applications/search.do?action=advanced&searchType=Application")
-st.selectbox("Local Authority",["Dumfries and Galloway","Highland"])
+dumgal = "https://eaccess.dumgal.gov.uk/online-applications/search.do?action=advanced&searchType=Application"
+highland = "https://wam.highland.gov.uk/wam/search.do?action=advanced"
+argbute = "https://publicaccess.argyll-bute.gov.uk/online-applications/search.do?action=advanced"
+st.selectbox("Local Authority",["Dumfries and Galloway","Highland","Argyll and Bute"])
+if st.button('Open browser'):
+    webbrowser.open_new_tab(dumgal)
