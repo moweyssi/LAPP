a29dcbc6f3495136a65af9d2636d9323d8b099dd
Maxim <maxim.oweyssi@est.org.uk>
2022-12-21 18:34:48 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-dumgal = "https://eaccess.dumgal.gov.uk/online-applications/search.do?action=advanced&searchType=Application")
+dumgal = ("https://eaccess.dumgal.gov.uk/online-applications/search.do?action=advanced&searchType=Application")
 st.selectbox("Local Authority",["Dumfries and Galloway","Highland"])
